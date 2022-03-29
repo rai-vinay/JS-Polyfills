b332979f4f2a27bed4efcb19ecac8874eb76809f
rai-vinay <vin13.rai@gmail.com>
2022-03-29 16:07:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ffe38c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ffe38c)
---
+++ index.js
@@ -0,0 +1,25 @@
+// // Import stylesheets
+// import './style.css';
+//  class User{
+//    constructor(email, name){
+//     this.email = email;
+//     this.name = name;
+//    }
+//    login(){
+//      console.log(this.email +"logged in");
+//    }
+//  }
+//  var userOne = new User('vin13.rai@gmail.com','vinay');
+//  var userTwo = new User('nish@gmail','nisha');
+//  console.log(userOne.login());
+//  console.log(userOne);
+//  userTwo.login();
+//  //Event delegation
+let arr = [
+  [1,2],
+  [3,4],
+  [5,6,[7,8],9],
+  [10,11,12]
+];
+ function CustomF
+ 
