dd4de8e52de56a0c9b8b920ec600041d0cef501e
rai-vinay <vin13.rai@gmail.com>
2022-03-29 17:15:10 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
-// // Import stylesheets
-// import './style.css';
-//  class User{
-//    constructor(email, name){
-//     this.email = email;
-//     this.name = name;
-//    }
-//    login(){
-//      console.log(this.email +"logged in");
-//    }
-//  }
-//  var userOne = new User('vin13.rai@gmail.com','vinay');
-//  var userTwo = new User('nish@gmail','nisha');
-//  console.log(userOne.login());
-//  console.log(userOne);
-//  userTwo.login();
+//++++++++++++++++++++++ CALL APPLY BIND+++++++++++++++++++++
+// var employee = {
+//   name: 'vinay',
+//   hello: function (thing) {
+//     console.log(this.name + ' says hello ' + thing);
+//   },
+// };
+// var alterEgo = {
+//   name: 'bunny',
+// };
+// employee.hello.call(alterEgo, 'world');
+// employee.hello.apply(alterEgo, ['India']);
+// const hello = employee.hello.bind(alterEgo);
+// hello('USA');
 //  //Event delegation
-let arr = [
-  [1,2],
-  [3,4],
-  [5,6,[7,8],9],
-  [10,11,12]
-];
- function CustomF
- 
+
+// ++++++++++++++++++++++++++++++++++++++++++++Custom Flattening
+// let arr = [
+//   [1, 2],
+//   [3, 4],
+//   [5, 6, [7, 8], 9],
+//   [10, 11, 12],
+// ];
+// function CustomFlat(arr, depth) {
+//   let result = [];
+//   arr.forEach((ar) => {
+//     if (Array.isArray(ar) && depth > 0) {
+//       result.push(...CustomFlat(ar, depth - 1));
+//     } else result.push(ar);
+//   });
+//   return result;
+// }
+// let x = CustomFlat(arr, 2);
+// console.log(x);
+// ++++++++++++++++++++++++++++++++++++++++++++CUSTOM COMPOSITION ++++++++++++++
+function addFive(a) {
+  return a + 5;
+}
+function subtractTwo(a) {
+  return a - 2;
+}
+const compose = (...functions) => {
+  return (args) => {
+    return functions.reduceRight((args, fn) => fn(args), args);
+  };
+};
+
+const evaluate = compose(addFive, subtractTwo);
+console.log(evaluate(5));
